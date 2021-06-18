0f8f87bd88e5faa36754365f56d9662b1dce574a
030030 <74403092+030030@users.noreply.github.com>
2021-06-18 21:25:17 +0800

    Update Button_Control.aspx.cs

--- 5-1Practice/Button_Control.aspx.cs
+++ 5-1Practice/Button_Control.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +9,11 @@ namespace _5_1Practice {
     public partial class Button_Control : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
 
+        }
+
+        protected void btn_Sub_Click(object sender, EventArgs e)
+        {
+            lb_Txt.Text = "這是個按鈕觸發Label標籤動作";
         }
     }
 }
